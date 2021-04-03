6ce46d27436470a7cc6c5d5a553315ab7f95b39a
BEZALEEL <anandmikkili@gmail.com>
2021-04-03 18:33:34 +0530

    Committing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values (1001,'USD','INR',72,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values (1002,'AUD','INR',40,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values (1003,'EUR','INR',95,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values (1004,'CAD','INR',56,'');
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple,environment) values (1005,'SGD','INR',45,'');
